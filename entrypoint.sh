451ac24008efa418abe65d5e4665c4e954d7f144
Jim Brancheau <jim.brancheau@daysmart.com>
2022-05-31 23:25:15 -0400

    Envvars no profile (#1)
    * remove profile setup, use env vars instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-newrelic profiles add --profile githubActions \
-  --licenseKey ${NEW_RELIC_INGEST_LICENSE_KEY} \
-  --apiKey ${NEW_RELIC_INGEST_LICENSE_KEY} \
-  --region "${NEW_RELIC_REGION}" \
-  --accountId ${NEW_RELIC_ACCOUNT_ID}
-
-newrelic profile default --profile githubActions
+export NEW_RELIC_API_KEY=dummy_key
+export NEW_RELIC_REGION=${NEW_RELIC_REGION}
+export NEW_RELIC_LICENSE_KEY=${NEW_RELIC_INGEST_LICENSE_KEY}
 
 result=$(newrelic reporting junit \
   --accountId "${NEW_RELIC_ACCOUNT_ID}" \
